51a910d36cc436e5eaffdfca038ace4c27555d27
lonhon <lonhontop@gmail.com>
2019-12-19 15:03:34 +0800

    feat: .md中图片标签图片下载&上传到新图床功能

---
+++ imgHandler.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const path = require('path')
+const request = require('request')
+const smms = require('smms-cli')
+
+async function imgMover (imgUri) {
+  const localImgFilePath = await downloader(imgUri)
+  const newImgUri = await upload(localImgFilePath)
+  if (!newImgUri) {
+    // TODO: 保存数据   便于二次上传
+    return null
+  }
+  console.log('newImgUri: ', newImgUri)
+  return {
+    oldUri: imgUri,
+    newUri: newImgUri
+  }
+}
+
+function downloader (url) {
+  return new Promise((resolve, reject) => {
+    const arr = url.split('/')
+    const fileName = arr[arr.length - 1];
+    const filePath = path.join('./imgs/test1-' + fileName)
+    request(url, {encoding: 'binary'}, (err, d) => {
+      if (err) return console.log('err: 下载图片失败.')
+      if (true) {
+        try{
+          fs.writeFileSync(filePath, d.body, 'binary')
+          resolve(filePath)
+        } catch (err) {
+          console.log('err: 保存图片失败.')
+          reject(null)
+        }
+      }
+    })
+  })
+}
+
+async function upload(imgPath) {
+  let newPath = null
+  try {
+    const res = await smms.upload(imgPath)
+    newPath = res.data.url
+    console.log('success: 上传成功,', newPath)
+  } catch (err) {
+    console.log('err: 上传失败,', err)
+  }
+  return newPath
+}
+
+module.exports = imgMover
--- scaner.js
+++ scaner.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 const path = require('path')
-import imgMover from './imgHander.js'
+const imgMover = require('./imgHandler')
 
 
-const folderPath = '/Users/lonhon/Documents/lonhon.top/source/_posts/'
+// const folderPath = '/Users/lonhon/Documents/lonhon.top/source/_posts/'
+const folderPath = './md/'
 const targetFolder = path.join(folderPath)
 
 const fileType = /.(md)$/
@@ -15,7 +16,7 @@ function scanFile (folder) {
       data.forEach(fileName => {
         if (fileType.test(fileName)) {
           const mdFile = path.join(folder + fileName)
-          checkFile(mdFile)
+          checkFile(mdFile, fileName)
         }
       })
     }
@@ -28,17 +29,29 @@ const imgReg = /<img.*?(?:>|\/>)/gi
 const srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i
 const uriReg = /^src="http*/
 
-function checkFile (file) {
+function checkFile (file, fname) {
   fs.readFile(file, 'utf8', (err, d) => {
     if (err) return console.log(`读取目标:${file}失败！`)
+    const transQueue = []
+    let count = 0
     const matchs = d.match(imgReg)
     matchs && matchs.forEach(async (el) => {
       const srcMatched = el.match(srcReg)
       if (srcMatched[0] && uriReg.test(srcMatched[0])) {
         const imgUri = srcMatched[0].substring(5, srcMatched[0].length - 1)
-        const newImgUri = await imgMover(imgUri)
-        console.log(newImgUri)
+        console.log(imgUri)
+        const transfer = await imgMover(imgUri)
+        count++
+        if (transfer) transQueue.push(transfer)
+        if (count === matchs.length) {
+          replacer(fname, transQueue)
+        }
+        // console.log(newImgUri)
       }
     })
   })
+}
+
+function replacer (fname, task) {
+  console.log(`${fname} img count: ` + task.length)
 }
